feefee0b3eaf246bc28d2d91928dc55b609c2069
FriendlessWyvern <Luccas-S@users.noreply.github.com>
2023-09-18 21:23:36 -0300

    Update tpchapter5.py

---
+++ tpchapter5.py
@@ -0,0 +1,15 @@
+"""
+O operador de divisão pelo piso,
+"//", divide dois números e arredonda o resultado para 
+um número inteiro para baixo.
+
+Neste exemplo fazemos um cálculo para transformar os minutos em horas,
+feito isso pegamos o restante de minutos e adicionamos ao número de horas.
+
+minutes = 105
+hours = minutes // 60
+
+remainder = minutes % 60
+
+print(hours, ":", remainder)
+"""
