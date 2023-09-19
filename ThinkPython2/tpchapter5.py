cb099985990c0f169c80baac49274c7862042e38
FriendlessWyvern <Luccas-S@users.noreply.github.com>
2023-09-19 16:46:34 -0300

    Arquivos de estudo.
    Capítulos do livro Pense Python 2.

---
+++ ThinkPython2/tpchapter5.py
@@ -0,0 +1,33 @@
+"""
+===Condicionais e Recursividade===
+
+DIVISÃO PELO PISO E MÓDULO
+
+"O operador de divisão pelo piso,
+"//", divide dois números e arredonda o resultado para 
+um número inteiro para baixo."
+
+Neste exemplo fazemos um cálculo para transformar os minutos em horas,
+feito isso pegamos o restante de minutos e adicionamos ao número de horas.
+
+minutes = 105
+hours = minutes // 60
+
+remainder = minutes % 60
+
+print(hours, ":", remainder)
+
+Operador Módulo = %.
+"O operador módulo é mais útil do que parece. 
+Por exemplo, é possível verificar se um número é divisível por outro
+-> se x % y for zero, então x é divisível por y.
+
+Além disso, você pode extrair o dígito ou dígitos mais à direita de um número. 
+Por exemplo, x % 10 produz o dígito mais à direita de x (na base 10). 
+Da mesma forma x % 100 produz os dois últimos dígitos."
+
+EXPRESSÕES BOOLEANAS
+
+
+
+"""
