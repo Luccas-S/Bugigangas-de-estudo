5c65304ad4d1ff437b87ea3847ace7e7ee1a9c5e
FriendlessWyvern <Luccas-S@users.noreply.github.com>
2023-11-09 15:46:52 -0300

    Update tpchapter9.py

---
+++ ThinkPython2/tpchapter9.py
@@ -0,0 +1,10 @@
+"""
+===Estudo de caso: jogos de palavras===
+
+LEITURA DE LISTAS DE PALAVRAS
+
+A função integrada open recebe o nome do arquivo como um parâmetro e 
+retorna um objeto de arquivo que você pode usar para ler o arquivo.
+
+>>> fin = open('words.txt')
+"""
